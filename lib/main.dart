99d3d09e8efbf956c24ad219b329b824322f7edc
mhddiab <87436690+mhddiab@users.noreply.github.com>
2021-07-14 18:58:42 +0300

    third update

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "My flutter app updatedgdfsd ",
+      title: "My flutter app updatedgdfsd sgggggg ",
       theme: ThemeData(
         primaryColor: Colors.red,
       ),
